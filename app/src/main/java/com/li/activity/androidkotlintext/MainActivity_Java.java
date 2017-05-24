7ade5178265b975322b0ba55dfbc4a044d72c751
li xiang <xiang.li@okkuaixiu.com>
2017-05-24 16:43:28 +0800

    完善项目配置
    添加对应的java文件用来对比;
    配置gradle

---
+++ app/src/main/java/com/li/activity/androidkotlintext/MainActivity_Java.java
@@ -0,0 +1,18 @@
+package com.li.activity.androidkotlintext;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by okkuaixiu on 2017/5/24.
+ */
+
+public class MainActivity_Java extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
